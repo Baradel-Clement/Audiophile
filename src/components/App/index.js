8da187eeeb311a707c43eda428b588a3848e6795
Clément Baradel <baradelclement@gmail.com>
2021-07-01 10:49:16 +0200

    Homepage hero done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import NavBar from '../NavBar';
+import Homepage from '../Homepage';
 
 const App = () => (
   <div className="app">
     <NavBar />
+    <Route path="/" exact>
+      <Homepage />
+    </Route>
   </div>
 );
 
---
+++ src/components/Homepage/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Homepage = () => (
+  <div className="Homepage">
+    <div className="Homepage-Hero">
+      <div className="Homepage-Hero-wrap">
+        <div className="Homepage-Hero-desc">
+          <p className="overline">NEW PRODUCT</p>
+          <h1>XX99 Mark II Headphones</h1>
+          <p>Experience natural, lifelike audio and exceptional
+            build quality made for the passionate music enthusiast.
+          </p>
+          <button type="button" className="button button1">SEE PRODUCT</button>
+        </div>
+        <div className="Homepage-Hero-image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Homepage;
