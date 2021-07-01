37958782d297d90c4a8293cf7a47f6bf4fd17a0b
Clément Baradel <baradelclement@gmail.com>
2021-07-01 17:14:36 +0200

    headline product

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -18,6 +18,34 @@ const Homepage = () => (
       </div>
     </div>
     <Categories marginTop={120} />
+    <div className="HeadlineProducts">
+      <div className="HeadlineProducts-ZX9">
+        <div className="HeadlineProducts-ZX9-illu" />
+        <svg id="HeadlineProducts-ZX9-circles" width="944" height="944" xmlns="http://www.w3.org/2000/svg"><g stroke="#FFF" fill="none" fillRule="evenodd" opacity=".202"><circle cx="472" cy="472" r="235.5" /><circle cx="472" cy="472" r="270.5" /><circle cx="472" cy="472" r="471.5" /></g></svg>
+        <div className="HeadlineProducts-ZX9-desc">
+          <h1>ZX9 SPEAKER</h1>
+          <p>Upgrade to premium speakers that are phenomenally
+            built to deliver truly remarkable sound.
+          </p>
+          <button type="button" className="button button2-reverse">SEE PRODUCT</button>
+        </div>
+      </div>
+      <div className="HeadlineProducts-ZX7">
+        <div className="HeadlineProducts-ZX7-desc">
+          <h4>ZX7 SPEAKER</h4>
+          <button className="button button2" type="button">SEE PRODUCT</button>
+        </div>
+      </div>
+      <div className="HeadlineProducts-YX1">
+        <div className="HeadlineProducts-YX1-illu" />
+        <div className="HeadlineProducts-YX1-desc">
+          <div className="HeadlineProducts-YX1-desc-wrap">
+            <h4>YX1 EARPHONES</h4>
+            <button className="button button2" type="button">SEE PRODUCT</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
